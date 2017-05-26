4329bc3ef978ccdf5f2425535b6db9da1ef27b6c
Becky Hall <uncoolplane@gmail.com>
2017-05-26 17:04:33 -0600

    fixed overwrite of changes to index.js

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const express = require('express');
 const session = require('express-session')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const config = require('./config');
+const config = require('./config');
 
-
+//Database
 const massive = require('massive');
-let connectionString = process.env.DATABASE_URL || "postgres://postgres:postgres@localhost/neighborhood";
+let connectionString = process.env.DATABASE_URL || config.connectionString ||  "postgres://postgres:postgres@localhost/neighborhood";
 let massiveInstance = massive.connectSync({connectionString})
 
+//API Routes
+const types = require('./routes/type');
+const users = require('./routes/users');
+const events = require('./routes/events');
+const neighborhoods = require('./routes/neighborhoods');
+
+//Application
 var app = module.exports = express();
 app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(session({
-  secret: process.env.SESSIONSECRET,
+  secret: process.env.SESSIONSECRET || config.sessionSecret,
   resave: false,
   saveUninitialized: true
  }));
@@ -23,14 +30,14 @@ app.use(cors());
 app.set('db', massiveInstance);
 var db = app.get('db');
 
-let testCtrl = require('./serverCtrls/testCtrl');
-// let cartCtrl = require('./controllers/cartCtrl');
-// let stripeCtrl = require('./controllers/stripeCtrl');
-// let orderCtrl = require('./controllers/orderCtrl');
+//API will be http://localhost/api/types, http://localhost/api/users, http://localhost/api/events, http://localhost:3000/api/neighborhoods etc. by using the prefix
+// let testCtrl = require('./serverCtrls/testCtrl');
+app.use('/api', types);
+app.use('/api', users);
+app.use('/api', events);
+app.use('/api', neighborhoods);
 
-// app.post('/api/cart/', testCtrl.something );
-
-const port = 3000
-app.listen(process.env.PORT || port, () => {
+const port = process.env.PORT || config.PORT || 3000;
+app.listen(port, () => {
   console.log(`sup from port ${port}`);
 })
