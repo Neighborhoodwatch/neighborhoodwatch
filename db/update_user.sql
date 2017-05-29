d3e42acc38b5b7807b9bf9d00e194caf9355db96
spencer <spencajones@gmail.com>
2017-05-29 13:05:21 -0600

    small changes to return data on an update

--- db/update_user.sql
+++ db/update_user.sql
@@ -1,3 +1,4 @@
 UPDATE users
 	SET first_name=$1, last_name=$2, username=$3, email=$4, facebook_id=$5, google_id=$6, password=$7, photo=$8
-	WHERE user_id=$9;
+	WHERE user_id=$9
+    RETURNING first_name, last_name, username, email, facebook_id, google_id, photo;
