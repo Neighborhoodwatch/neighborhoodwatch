3be82e0604810ff7bef741b5d4f6037e18593931
Becky Hall <uncoolplane@gmail.com>
2017-05-30 20:27:11 -0600

    added search for neighborhoods

--- db/get_neighborhoods.sql
+++ db/get_neighborhoods.sql
@@ -1,1 +1,5 @@
-select * from neighborhood;
+select * from neighborhood
+where
+  neighborhood.city like '%$1%' or $1 is null OR
+  neighborhood.state like '%$2%' or $2 is null OR
+  neighborhood.name like '%$3%' or $3 is null
