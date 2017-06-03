2c59f29917651f2c338207c080c018dbabe47562
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-06-03 15:44:16 -0600

    reverted back even though its not the best way to go. will add proper changes later.

--- public/app/views/createEvent/createEventCtrl.js
+++ public/app/views/createEvent/createEventCtrl.js
@@ -16,46 +16,47 @@ angular.module('nWatch').controller('createEventCtrl', function($scope, eventSrv
     })
   }
 
-  // $scope.lists = [
-  //   {
-  //     name: 'Lost Pet',
-  //     type_id: 1
-  //   },
-  //   {
-  //     name: 'Damage',
-  //     type_id:2
-  //   },
-  //   {
-  //     name: 'Neighborhood Watch',
-  //     type_id:3
-  //   },
-  //   {
-  //     name: 'Clean-up',
-  //     type_id:4
-  //   },
-  //   {
-  //     name: 'Missing Person',
-  //     type_id:5
-  //   },
-  //   {
-  //     name: 'Meet Up',
-  //     type_id:6
-  //   },
-  //   {
-  //     name: 'Entertainment',
-  //     type_id: 7
-  //   },
-  //   {
-  //     name: 'Other',
-  //     type_id:8
-  //   }
-  // ]
+  $scope.lists = [
+    {
+      name: 'Lost Pet',
+      type_id: 1
+    },
+    {
+      name: 'Damage',
+      type_id: 2
+    },
+    {
+      name: 'Neighborhood Watch',
+      type_id: 4
+    },
+    {
+      name: 'Clean-up',
+      type_id: 5
+    },
+    {
+      name: 'Missing Person',
+      type_id: 6
+    },
+    {
+      name: 'Meet Up',
+      type_id: 7
+    },
+    {
+      name: 'Entertainment',
+      type_id: 8
+    },
+    {
+      name: 'Other',
+      type_id: 3
+    }
+  ]
 
-  typeService.getTypes().then(function (res, err) {
-    console.log('types', res, err);
-    $scope.lists = res.data;
+  // typeService.getTypes().then(function (res, err) {
+  //   console.log('types', res, err);
+  //   $scope.lists = res.data;
     $scope.category = $scope.lists[0]
-  })
+  //
+  // })
 
   $scope.eventImg = "yoyoyo"
 
