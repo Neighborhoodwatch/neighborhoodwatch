5923ee8bdcc68fb85b774b60bf0bd7aafcab3c10
Becky Hall <uncoolplane@gmail.com>
2017-06-05 20:17:57 -0600

    rollback event ctrl

--- public/app/views/createEvent/createEventCtrl.js
+++ public/app/views/createEvent/createEventCtrl.js
@@ -110,54 +110,6 @@ angular.module('nWatch').controller('createEventCtrl', function($scope, eventSrv
     console.log(event);
     eventSrvc.save(event)
   }
-
-  //photo upload
-  $scope.file = {};
-  $scope.message = false;
-  $scope.alert = '';
-  $scope.defaultUrl = 'app/img/solitary-weed.png';
-
-  $scope.Submit = function() {
-      $scope.uploading = true;
-      uploadFile.upload($scope.file).then(function(data) {
-          if (data.data.success) {
-              $scope.uploading = false;
-              $scope.alert = 'alert alert-success';
-              $scope.message = data.data.message;
-              $scope.file = {};
-          } else {
-              $scope.uploading = false;
-              $scope.alert = 'alert alert-danger';
-              $scope.message = data.data.message;
-              $scope.file = {};
-          }
-      });
-  };
-
-  $scope.photoChanged = function(files) {
-      if (files.length > 0 && files[0].name.match(/\.(png|jpeg|jpg)$/)) {
-          $scope.uploading = true;
-          var file = files[0];
-          var fileReader = new FileReader();
-          fileReader.readAsDataURL(file);
-          fileReader.onload = function(e) {
-              $timeout(function() {
-                  $scope.thumbnail = {};
-                  $scope.thumbnail.dataUrl = e.target.result;
-                  if(!$scope.photo) {
-                    $scope.photo = 'app/img/' + file.name || $scope.defaultUrl;
-                  }
-                  $scope.uploading = false;
-                  $scope.message = false;
-              });
-          };
-      } else {
-          $scope.thumbnail = {};
-          $scope.message = false;
-      }
-  };
-  //end photo upload
-
   // ui--bootstrap date js
   $scope.today = function() {
     $scope.dt = new Date();
